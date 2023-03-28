13d9d292298815d419850a7ccc84226a1d136813
TellSamm <chernikov.semen21@gmail.com>
2023-03-28 14:12:18 +0300

    mini changes

--- src/test/java/tests/RegistrationWithFakerTests.java
+++ src/test/java/tests/RegistrationWithFakerTests.java
@@ -1,9 +1,7 @@
 package tests;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static tests.TestData.*;
 import static utils.RandomUtils.getRandomItemFromArray;
 
