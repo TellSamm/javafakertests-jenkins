08937007eeb2643b5dd36dfe53aea78bc2063dd1
TellSamm <chernikov.semen21@gmail.com>
2023-04-12 23:02:43 +0300

    mini changes-2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,12 +22,12 @@ public class TestBase {
 
     @BeforeAll
     static void openPracticeForm() {
-        //Configuration.browserSize = "1920x1080";
-        //Configuration.baseUrl = "https://demoqa.com";
-       // Configuration.browser = "chrome"; // браузер
-        //Configuration.browserVersion = "100.0"; // версия браузера
-        //Configuration.holdBrowserOpen = false;
-       // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //удалённый запуск браузера на selenoid
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome"; // браузер
+        Configuration.browserVersion = "100.0"; // версия браузера
+        Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //удалённый запуск браузера на selenoid
 
         //конфиг что бы добавилось enableVNC - это мы включаем что бы было окошко в окошке в Selenoid
         //, enableVideo - вкл.запись видео происходит + ниже есть настройка Attach.addVideo(); // ЗАПИСЬ ВИДЕО
